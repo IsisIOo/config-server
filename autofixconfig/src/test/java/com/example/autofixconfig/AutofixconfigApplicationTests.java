0664d9485ae056bcede4c1261db57c4280829692
Isidora <isidora.oyanedel@usach.cl>
2024-05-15 18:28:39 -0400

    inicio de orden
    trato de crear los archivos necesarios para arrancar este proyecto

---
+++ autofixconfig/src/test/java/com/example/autofixconfig/AutofixconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autofixconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofixconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
