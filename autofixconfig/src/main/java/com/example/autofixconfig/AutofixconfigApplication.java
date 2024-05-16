4fc346aa728d97ee107e022bcd0dabc1fed255c2
Isidora <isidora.oyanedel@usach.cl>
2024-05-15 23:26:44 -0400

    joge

--- autofixconfig/src/main/java/com/example/autofixconfig/AutofixconfigApplication.java
+++ autofixconfig/src/main/java/com/example/autofixconfig/AutofixconfigApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigServer
 public class AutofixconfigApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AutofixconfigApplication.class, args);
     }
